5c6b3b145633a0693af4e1cc7a61990ff9cd343e
akmindaugas <mindaugas.akelis@gmail.com>
2024-02-09 21:58:51 +0200

    commit form set

--- item_card/item.js
+++ item_card/item.js
@@ -1,23 +1,33 @@
 const title = document.getElementById('title');
 const price = document.getElementById('price');
 const imgUrl = document.getElementById('imgUrl');
+const locationInput = document.getElementById('location');
+const description = document.getElementById('description');
+const email = document.getElementById('email');
+// let date = document.getElementById('date');
+
 const button = document.getElementById('btn'); 
 const  message = document.getElementById('message'); 
 
 button.addEventListener('click', async () => {
-  console.log('click')
-    if(!title.value || !price.value) {
-    message.innerText = 'complete the form';
-return};
-
+  console.log('add button click')
+  if (!title.value || !price.value || !locationInput.value || !description.value || !/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(email.value)) {
+    message.innerText = 'all slots fill required';
+    return;
+  }
 
     const itemData = {
         title: title.value,
         price: price.value,
-        imgUrl: imgUrl.value
+        img_url: imgUrl.value,
+        location: locationInput.value,
+        description: description.value,
+        email: email.value,
+        // date: Date.now(),
     };
+console.log(itemData);
 
-    const response = fetch('https://sophisticated-humane-dandelion.glitch.me', {
+    const response = await fetch('https://65bb510052189914b5bbb7b1.mockapi.io', {
         method: 'POST',
         headers: {
             Accept: 'application/json',
@@ -36,5 +46,5 @@ setTimeout(() => {
 }, 1000);
 console.log(addedItem)
   
-    });
+});
 
